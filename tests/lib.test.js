81058fb7cde33f4f6c99e61b357b0e0c581e74dc
David Olumbe <david.olumbe@quartix.net>
2018-07-26 17:02:36 +0100

    passed

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -6,8 +6,8 @@ test('absolute - should return positive number if input is +ve', () => {
     expect(result).toBe(1);
 });
 
-test('absolute - should return negative number if input is -ve', () => {
+test('absolute - should return positive number if input is -ve', () => {
     const result = lib.abosulte(-1);
 
-    expect(result).toBe(-1);
+    expect(result).toBe(1);
 });
