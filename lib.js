ffe65148b6c17ee0fc3e29758ee640b6ad413b60
David Olumbe <david.olumbe@quartix.net>
2018-07-27 11:27:35 +0100

    refractor without breaking test

--- lib.js
+++ lib.js
@@ -3,9 +3,8 @@
 
 //Testing number
 module.exports.abosulte = (number) => {
-    if (number > 0) return number;
-    if (number < 0) return -number;
-    return 0;
+    if (number >= 0) return number;
+        return -number;
 }
 
 // //Testing strings
