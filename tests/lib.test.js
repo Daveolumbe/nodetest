1bc423e916050980a57f99a6ce685e2ccdefa2a5
David Olumbe <david.olumbe@quartix.net>
2018-07-27 11:26:05 +0100

    group test

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,13 +1,19 @@
 const lib = require('../lib');
 
-test('absolute - should return positive number if input is +ve', () => {
-    const result = lib.abosulte(1);
 
+describe('abosulte', () => {
+it('should return positive number if input is +ve', () => {
+    const result = lib.abosulte(1);
     expect(result).toBe(1);
 });
 
-test('absolute - should return positive number if input is -ve', () => {
+it('should return positive number if input is +ve', () => {
     const result = lib.abosulte(-1);
-
     expect(result).toBe(1);
+});
+
+it('should return zero number if input is zero', () => {
+    const result = lib.abosulte(0);
+    expect(result).toBe(0);
+});  
 });
